389144685e75773b26936bc949c3a722311a44f4
Lukin <maximlm93@gmial.com>
2024-10-06 11:51:33 +0500

    add checkstyle plugin and jackson dependecy

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("java")
     application
+    checkstyle
 }
 
 application {
@@ -15,6 +16,7 @@ repositories {
 }
 
 dependencies {
+    implementation ("com.fasterxml.jackson.core:jackson-databind:2.18.0")
     implementation ("info.picocli:picocli:4.7.6")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
