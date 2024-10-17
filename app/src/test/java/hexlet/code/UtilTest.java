8e6b2573179046cdc2f919a36c699313498b40aa
Lukin <maximlm93@gmial.com>
2024-10-17 18:47:50 +0500

    fixed checkstyle error

--- app/src/test/java/hexlet/code/UtilTest.java
+++ app/src/test/java/hexlet/code/UtilTest.java
@@ -15,7 +15,12 @@ public class UtilTest {
         var filePath1 = "src/test/resources/fixtures/file1.json";
         var filePath2 = "src/test/resources/fixtures/file2.yaml";
 
-        var expected1 = "{\r\n  \"host\": \"hexlet.io\",\r\n  \"timeout\": 50,\r\n  \"proxy\": \"123.234.53.22\",\r\n  \"follow\": false\r\n}";
+        var expected1 = "{\r\n"
+                + "  \"host\": \"hexlet.io\",\r\n"
+                + "  \"timeout\": 50,\r\n"
+                + "  \"proxy\": \"123.234.53.22\",\r\n"
+                + "  \"follow\": false\r\n"
+                + "}";
         var actual1 = readFile(filePath1);
         var expected2 = "timeout: 20\r\nverbose: true\r\nhost: hexlet.io";
         var actual2 = readFile(filePath2);
