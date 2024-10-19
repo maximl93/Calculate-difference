087ab78eab613571152c3e8cba8a83b2f00c4963
Lukin <maximlm93@gmial.com>
2024-10-19 18:37:36 +0500

    removed \r symbols

--- app/src/test/java/hexlet/code/UtilTest.java
+++ app/src/test/java/hexlet/code/UtilTest.java
@@ -15,14 +15,14 @@ public class UtilTest {
         var filePath1 = "src/test/resources/fixtures/file1.json";
         var filePath2 = "src/test/resources/fixtures/file2.yaml";
 
-        var expected1 = "{\r\n"
-                + "  \"host\": \"hexlet.io\",\r\n"
-                + "  \"timeout\": 50,\r\n"
-                + "  \"proxy\": \"123.234.53.22\",\r\n"
-                + "  \"follow\": false\r\n"
+        var expected1 = "{\n"
+                + "  \"host\": \"hexlet.io\",\n"
+                + "  \"timeout\": 50,\n"
+                + "  \"proxy\": \"123.234.53.22\",\n"
+                + "  \"follow\": false\n"
                 + "}";
         var actual1 = readFile(filePath1);
-        var expected2 = "timeout: 20\r\nverbose: true\r\nhost: hexlet.io";
+        var expected2 = "timeout: 20\nverbose: true\nhost: hexlet.io";
         var actual2 = readFile(filePath2);
 
         assertEquals(expected1, actual1);
