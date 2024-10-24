53c9de47d41c1d6d5b1cd91ab57b76a426a0f4f5
Maksim Lukin <maximlm93@gmail.com>
2024-10-24 16:01:15 +0500

    fixed checkstyle error

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,7 +1,13 @@
 package hexlet.code;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.LinkedHashMap;
+import java.util.Optional;
 
 
 import static hexlet.code.Parser.getFileParser;
